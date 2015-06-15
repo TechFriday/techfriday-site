f28cf0a2d1758f29dcffb4c26f8c6832c4beaa71
Vadim Vinogradov <vadim.vinogradov@gmail.com>
2015-06-16 00:44:54 +0400

    Add static files serving support

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,28 @@
+// SETTINGS
+const PORT = 80;
+const MAX_AGE = '30d';
+const STATIC_FILES_FOLDER = 'client';
+const REST_API_ROOT_ENDPOINT = '/api';
+
 var express = require('express');
 var app = express();
 
-app.get('/node', function (req, res) {
-  res.send('Hello World!');
-});
 
-var server = app.listen(3000, function () {
+// STATIC FILES
+app.use(express.static(__dirname + '/../' + STATIC_FILES_FOLDER, {maxAge: MAX_AGE}));
+
+
+// REST API
+var router = express.Router();
+
+router.get('/example', function(request, response) {
+	response.json({foo: 'bar'});
+});
+app.use(REST_API_ROOT_ENDPOINT, router);
+
+
+
+var server = app.listen(PORT, function () {
 
   var host = server.address().address;
   var port = server.address().port;
